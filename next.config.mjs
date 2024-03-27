b8e1962e41dfda8e1da3ceee439745d435ee7e9a
H <744769098@qq.com>
2024-03-27 11:25:21 +0800

    增加操作的权限

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,7 @@
  * @Date: 2024-01-24 18:01:39
  * @Description: description
  */
-import createNextIntlPlugin from 'next-intl/plugin';
-const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output: 'export',
